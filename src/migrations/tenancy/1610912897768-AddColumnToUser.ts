80ea97fdec16056ae479310bafb724a7ce8e6564
Omar Marenco <omar@omarenco.com>
2021-01-17 17:53:39 -0500

    Agregando Campos a Put de User

---
+++ src/migrations/tenancy/1610912897768-AddColumnToUser.ts
@@ -0,0 +1,89 @@
+import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from 'typeorm';
+
+export class AddColumnToUser1610912897768 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumns('users', [
+      new TableColumn({
+        name: 'lastname',
+        type: 'varchar',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'picture',
+        type: 'varchar',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'phone',
+        type: 'varchar',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'gender',
+        type: 'varchar',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'linkeid',
+        type: 'varchar',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'country',
+        type: 'varchar',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'state',
+        type: 'varchar',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'city',
+        type: 'varchar',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'address',
+        type: 'varchar',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'zipcode',
+        type: 'varchar',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'identification_type_id',
+        type: 'int',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'identification',
+        type: 'varchar',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'terms_and_conditions',
+        type: 'bool',
+        isNullable: true,
+      }),
+      new TableColumn({
+        name: 'welcome_message',
+        type: 'bool',
+        default: true,
+      }),
+    ]);
+
+    await queryRunner.createForeignKey(
+      'users',
+      new TableForeignKey({
+        columnNames: ['identification_type_id'],
+        referencedColumnNames: ['id'],
+        referencedTableName: 'public.identification_types',
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
