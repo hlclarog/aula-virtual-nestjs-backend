6e9af961f8bceee7d48f962cb5ba99f8b45e6a45
hlclarog <hlclarog@gmail.com>
2021-04-03 16:42:12 -0500

    fix: filter tenancy module by tenancy id

--- src/api/acl/permissions/permissions.service.ts
+++ src/api/acl/permissions/permissions.service.ts
@@ -10,6 +10,10 @@ import { BaseRepo } from '../../../base/base.repository';
 import { Permissions } from './permissions.entity';
 import { TENANCY_MODULES_PROVIDER } from './../../tenancy_modules/tenancy_modules.dto';
 import { TenancyModules } from './../../tenancy_modules/tenancy_modules.entity';
+import {
+  INFO_TENANCY_PROVIDER,
+  InfoTenancyDomain,
+} from '../../../utils/providers/info-tenancy.module';
 
 @Injectable()
 export class PermissionsService extends BaseService<
@@ -20,13 +24,18 @@ export class PermissionsService extends BaseService<
   @Inject(PERMISSIONS_PROVIDER) repository: BaseRepo<Permissions>;
   @Inject(TENANCY_MODULES_PROVIDER)
   repositoryTenanciesModules: BaseRepo<TenancyModules>;
+  @Inject(INFO_TENANCY_PROVIDER) tenancy: InfoTenancyDomain;
 
   constructor() {
     super();
   }
 
   async findForTenancy(): Promise<Permissions[]> {
-    const modules = await this.repositoryTenanciesModules.find();
+    const modules = await this.repositoryTenanciesModules.find({
+      where: {
+        tenancy_id: this.tenancy.domain.tenancy_id,
+      },
+    });
     const modulesListIds = modules.map((module) => module.module_id);
     return await this.repository
       .createQueryBuilder('permission')
@@ -43,7 +52,7 @@ export class PermissionsService extends BaseService<
       ])
       .leftJoin('permission.module', 'module')
       .where(
-        `permission.id in (${
+        `module.id in (${
           modulesListIds.length > 0 ? modulesListIds.join() : [0].join()
         })`,
         {},
