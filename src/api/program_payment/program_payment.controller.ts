cfbacc2e42572337ec7437c5df5738cad187aa50
hlclarog <hlclarog@gmail.com>
2021-04-17 20:10:32 -0500

    feat: change dto

--- src/api/program_payment/program_payment.controller.ts
+++ src/api/program_payment/program_payment.controller.ts
@@ -2,7 +2,10 @@ import { Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
 import { ProgramPaymentService } from './program_payment.service';
 import { BaseController } from '../../base/base.controller';
 import { ProgramPayment } from './program_payment.entity';
-import { CreateProgramPaymentDto, UpdateProgramPaymentDto } from './program_payment.dto';
+import {
+  CreateProgramPaymentDto,
+  UpdateProgramPaymentDto,
+} from './program_payment.dto';
 import { ControllerApi } from '../../utils/decorators/controllers.decorator';
 
 @ControllerApi({ name: 'program_payment' })
@@ -11,8 +14,8 @@ export class ProgramPaymentController extends BaseController<
   CreateProgramPaymentDto,
   UpdateProgramPaymentDto
 > {
-  constructor(private readonly program_paymentService: ProgramPaymentService) {
-    super(program_paymentService);
+  constructor(private readonly programPaymentService: ProgramPaymentService) {
+    super(programPaymentService);
   }
   @Post()
   async post(@Body() createDto: CreateProgramPaymentDto) {
@@ -30,7 +33,10 @@ export class ProgramPaymentController extends BaseController<
   }
 
   @Put(':id')
-  async edit(@Param('id') id: string, @Body() updateDto: UpdateProgramPaymentDto) {
+  async edit(
+    @Param('id') id: string,
+    @Body() updateDto: UpdateProgramPaymentDto,
+  ) {
     return await this.update(id, updateDto);
   }
 
